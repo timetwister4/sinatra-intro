028fb20bd669bb915917dbac5299acb5afa13bdf
Anne Charnes <timetwister4@yahoo.com>
2016-09-16 16:40:31 -0400

    First commit

---
+++ config/environments.rb
@@ -0,0 +1,16 @@
+#The environment variable DATABASE_URL should be in the following format:
+# => postgres://{user}:{password}@{host}:{port}/path
+require 'zlib'
+require 'byebug'
+configure :production, :development do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://anne:password@localhost/test')
+	#byebug
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
